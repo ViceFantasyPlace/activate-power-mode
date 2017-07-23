a6ee6aa1c5b1bbc0b38c58bd40a6bbd47bad21cc
suika <lzk418901364@163.com>
2017-07-23 14:54:54 +0800

    整理action的重复代码

--- src/com/jiyuanime/action/ParticleSwitchAction.java
+++ src/com/jiyuanime/action/ParticleSwitchAction.java
@@ -11,35 +11,14 @@ import com.jiyuanime.config.Config;
  * <p>
  * Created by suika on 15-12-22.
  */
-public class ParticleSwitchAction extends AnAction {
-
-    Config.State state = Config.getInstance().state;
-
-    public ParticleSwitchAction() {
-        Presentation presentation = this.getTemplatePresentation();
-        if (state.IS_SPARK) {
-            presentation.setIcon(AllIcons.General.InspectionsOK);
-        } else {
-            presentation.setIcon(null);
-        }
+public class ParticleSwitchAction extends BaseSwitchAction {
+    @Override
+    boolean getSwitchFieldValue() {
+        return state.IS_SPARK;
     }
 
     @Override
-    public void actionPerformed(AnActionEvent e) {
-        if (state.IS_SPARK) {
-            this._disable(e.getPresentation());
-        } else {
-            this._enable(e.getPresentation());
-        }
-    }
-
-    private void _disable(Presentation presentation) {
-        state.IS_SPARK = false;
-        presentation.setIcon(null);
-    }
-
-    private void _enable(Presentation presentation) {
-        state.IS_SPARK = true;
-        presentation.setIcon(AllIcons.General.InspectionsOK);
+    void setSwitchFieldValue(boolean is_enable) {
+        state.IS_SPARK = is_enable;
     }
 }
